1f2b073344dd68fa66128ca8e994ee05585f29a4
hella753 <kristi.gaphrindashvili@gmail.com>
2024-11-14 02:45:05 +0400

    custom permission to allow only user who created the object to change it.

---
+++ restaurant/utils/permissions.py
@@ -0,0 +1,22 @@
+from rest_framework.permissions import BasePermission
+
+
+class IsTheUserWhoCreated(BasePermission):
+    """
+    Allows access only to the user who created the object.
+    """
+    def __init__(self, view_name):
+        self.view_name = view_name
+        super().__init__()
+
+
+    def has_permission(self, request, view):
+        if self.view_name == "restaurant":
+            user_who_created = view.get_object().user
+        elif self.view_name == "category":
+            user_who_created = view.get_object().restaurant.user
+        elif self.view_name == "subcategory":
+            user_who_created = view.get_object().parent.restaurant.user
+        elif self.view_name == "dish":
+            user_who_created = view.get_object().category.parent.restaurant.user
+        return bool(request.user == user_who_created and request.user.is_authenticated)
