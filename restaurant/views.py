a59aa1681797da0e13ea0d748213f226aff05956
hella753 <kristi.gaphrindashvili@gmail.com>
2024-11-14 14:33:17 +0400

    authorized user only has access to its objects without permissions.py. overrided get_queryset.

--- restaurant/views.py
+++ restaurant/views.py
@@ -11,10 +11,21 @@ from restaurant.utils.permissions import IsTheUserWhoCreated
 
 class CategoryViewSet(ModelViewSet):
     serializer_class = CategorySerializer
-    queryset = Category.objects.select_related(
-        "restaurant",
-        "restaurant__user"
-    )
+
+    def get_queryset(self):
+        if not self.request.user.is_authenticated:
+            queryset = Category.objects.select_related(
+                "restaurant",
+                "restaurant__user"
+            )
+        else:
+            queryset = Category.objects.filter(
+                restaurant__user=self.request.user
+            ).select_related(
+                "restaurant",
+                "restaurant__user"
+            )
+        return queryset
 
     def get_serializer_class(self):
         if self.action == "create" or self.action == "update":
@@ -37,14 +48,25 @@ class CategoryViewSet(ModelViewSet):
 
 class SubcategoryViewSet(ModelViewSet):
     serializer_class = SubcategorySerializer
-    queryset = Subcategory.objects.select_related(
-        "parent",
-        "parent__restaurant",
-        "parent__restaurant__user",
-    )
-
     filterset_class = CategoryFilter
     filter_backends = [CustomFilterBackend]
+
+    def get_queryset(self):
+        if not self.request.user.is_authenticated:
+            queryset = Subcategory.objects.select_related(
+                "parent",
+                "parent__restaurant",
+                "parent__restaurant__user",
+            )
+        else:
+            queryset = Subcategory.objects.filter(
+                parent__restaurant__user=self.request.user
+            ).select_related(
+                "parent",
+                "parent__restaurant",
+                "parent__restaurant__user",
+            )
+        return queryset
 
     def retrieve(self, request, *args, **kwargs):
         qs = Dish.objects.prefetch_related(
@@ -81,12 +103,25 @@ class SubcategoryViewSet(ModelViewSet):
 
 class DishViewSet(ModelViewSet):
     serializer_class = DishSerializer
-    queryset = Dish.objects.all().select_related(
-        "category",
-        "category__parent",
-        "category__parent__restaurant",
-        "category__parent__restaurant__user",
-    )
+
+    def get_queryset(self):
+        if not self.request.user.is_authenticated:
+            queryset = Dish.objects.select_related(
+                "category",
+                "category__parent",
+                "category__parent__restaurant",
+                "category__parent__restaurant__user",
+            )
+        else:
+            queryset = Dish.objects.filter(
+                category__parent__restaurant__user=self.request.user
+            ).select_related(
+                "category",
+                "category__parent",
+                "category__parent__restaurant",
+                "category__parent__restaurant__user",
+            )
+        return queryset
 
     def get_permissions(self):
         if self.action == "create":
@@ -109,7 +144,19 @@ class DishViewSet(ModelViewSet):
 
 class RestaurantViewSet(ModelViewSet):
     serializer_class = RestaurantSerializer
-    queryset = Restaurant.objects.select_related("user")
+
+    def get_queryset(self):
+        if not self.request.user.is_authenticated:
+            queryset = Restaurant.objects.select_related(
+                "user"
+            )
+        else:
+            queryset = Restaurant.objects.filter(
+                user=self.request.user
+            ).select_related(
+                "user"
+            )
+        return queryset
 
     def get_serializer_class(self):
         if self.action == "create" or self.action == "update":
